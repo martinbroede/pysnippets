0a430b1d3d4d93dfe485c3009c698f5d459653bb
Martin Broede <martinbro2021@yahoo.com>
2022-10-19 23:24:12 +0200

    format

--- examples/list_slicing.py
+++ examples/list_slicing.py
@@ -1,10 +1,12 @@
-# Clarifies behaviour of list slicing.
+# Clarifies behaviour of list slicing and indexing.
 # Keywords:
 # list / slicing / slice / fragmentation / index / indexing
 
 my_list = [1, 2, 3, 4, 5, 6, 7]
+
+print(my_list[0])  # 1
+print(my_list[-1])  # 7
+
 print(my_list[1:])  # [2, 3, 4, 5, 6, 7]
 print(my_list[:3])  # [1, 2, 3]
 print(my_list[3:5])  # [4, 5]
-
-print(my_list[-1])  # 7
--- examples/list_sorting.py
+++ examples/list_sorting.py
@@ -4,11 +4,15 @@
 
 my_list = [("a", 3), ("c", 1), ("b", 2)]
 # [('a', 3), ('c', 1), ('b', 2)]
+
 my_list.reverse()
 # [('b', 2), ('c', 1), ('a', 3)]
+
 my_list.sort(key=lambda s: s[0])  # same as my_list.sort() in this example.
 # [('a', 3), ('b', 2), ('c', 1)]
+
 my_list.sort(key=lambda s: s[1])  # lists are sorted ascending by default.
 # [('c', 1), ('b', 2), ('a', 3)]
+
 my_list.sort(key=lambda s: s[1], reverse=True)  # sort descending
 # [('a', 3), ('b', 2), ('c', 1)]
