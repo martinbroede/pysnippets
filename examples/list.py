d3e762ce42395b58ce64b9d6568e91543ee124fc
Martin Broede <martinbro2021@yahoo.com>
2022-10-19 22:09:03 +0200

    separate lines

--- examples/list.py
+++ examples/list.py
@@ -5,13 +5,16 @@
 my_list = ['A', 1]  # list elements can have arbitrary types
 my_list += ['a', 'Z']  # you can concatenate lists like strings
 # the line above equals my_list.extend(['a', 'Z'])
+
 print(my_list.pop())  # pop last item of list (standard pop behaviour in LIFO structures)
 # Z
 print(my_list.pop(0))  # pop first item of list
 # A
+
 my_list.append("last")
 my_list.insert(0, "first")  # 'prepend'
 my_list.remove('a')
+
 print(my_list)
 # ['first', 1, 'last']
 print(my_list * 2)  # concatenate list with itself
