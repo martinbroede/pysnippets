0652f97e59cef2bb48e00b957f4f63863c442a78
Martin Broede <martinbro2021@yahoo.com>
2022-10-27 09:55:58 +0200

    add builtin sorted

--- examples/list_sorting.py
+++ examples/list_sorting.py
@@ -1,6 +1,8 @@
 # Clarifies behaviour of lists regarding sort and reverse methods.
 # Keywords:
-# list / sort / key / lambda / reverse / ascending / descending
+# list / sort / sorted / key / lambda / reverse / ascending / descending
+
+# Note that my_list.sort() and my_list.reverse() change the original list while returning None.
 
 my_list = [("a", 3), ("c", 1), ("b", 2)]
 # [('a', 3), ('c', 1), ('b', 2)]
@@ -11,8 +13,16 @@ my_list.reverse()
 my_list.sort(key=lambda s: s[0])  # same as my_list.sort() in this example.
 # [('a', 3), ('b', 2), ('c', 1)]
 
+
 my_list.sort(key=lambda s: s[1])  # lists are sorted ascending by default.
 # [('c', 1), ('b', 2), ('a', 3)]
 
 my_list.sort(key=lambda s: s[1], reverse=True)  # sort descending
+# [('a', 3), ('b', 2), ('c', 1)]
+
+# The builtin function sorted(iterable) returns a sorted list while not touching the original.
+sorted(my_list, reverse=True)
+# [('c', 1), ('b', 2), ('a', 3)]
+
+print(my_list)  # my_list stayed untouched.
 # [('a', 3), ('b', 2), ('c', 1)]
